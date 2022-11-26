8449704abfa74ebcb1c2de77057f7ffa197eea6c
NandoMCT <nandomct13@gmail.com>
2022-11-26 01:03:43 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ class Calculator {
     addDigit(digit) {
 
         //check if current operation already has a dot
-        if(digit === "." && this.currentOperationText.innerText.includes(".")) {
+        if (digit === "." && this.currentOperationText.innerText.includes(".")) {
             return;
         }
 
@@ -20,9 +20,43 @@ class Calculator {
         this.updateScreen();
     }
 
+
+    //Process to all calculator operations
+    processOperation(operation) {
+
+        //Get Current and Previous value
+        let operationValue;
+        const previous = +this.previousOperationText.innerText;
+        const current = +this.currentOperationText.innerText;
+
+        switch (operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            default:
+                return;
+        }
+
+    }
+
     //chance values of the calculator screen
-    updateScreen() {
-        this.currentOperationText.innerText += this.currentOperation;
+    updateScreen(operationValue = null, operation = null, current = null, previous = null) {
+
+        console.log(operationValue, operation, current, previous);
+
+        if (operationValue === null) {
+            this.currentOperationText.innerText += this.currentOperation;
+        }
+        else {
+            //check if value is zero, if it is just add current value
+            if (previous === 0) {
+                operationValue = current;
+            }
+            //Add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = "";
+        }
     }
 }
 
@@ -37,7 +71,7 @@ buttons.forEach((btn) => {
             calc.addDigit(value);
         }
         else {
-            console.log("OP: " + value);
+            calc.processOperation(value);
         }
     })
 })
